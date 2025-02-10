ac99cef24b9c6042322b7b9572651aecbf8c1aa8
SLaculeanu <76444893+SLaculeanu@users.noreply.github.com>
2025-02-10 08:42:45 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 20,
+        skills: ["JavaScript", "React", "Node"],
+        details: {
+            address: "New York",
+            USP: "Full Stack Developer"
+        }
+    };
+    return member;
+}
